22c74d988e6fb1f8eac37e98bcf32cc24c08c99d
smartx-usman <usman@smartx.kr>
2018-04-19 19:19:37 +0900

    Removed parameters values.

--- Visibility-Agent/IOVisor/transfer_iovisor_data.sh
+++ Visibility-Agent/IOVisor/transfer_iovisor_data.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-HOST='103.22.221.56'
-USER='netcs'
-PASS="fn!xo!ska!"
+#
+# Name          : transfer_iovisor_data.sh
+# Description   : A script for transferring network packets data to the SmartX Visibility Center
+#
+# Created by    : Muhammad Usman
+# Version       : 0.1
+# Last Update   : April, 2018
+
+#Modify these parameters before execution on SmartX Boxes
+#Also install sshpass and add SmartX Visibility Center IP for automatic logins.
+
+VISIBILITY_CENTER_IP=
+VISIBILITY_CENTER_USER=
+
 TARGETFOLDER='/home/netcs/IOVisor-Data/SmartX-Box-UM'
 SOURCEFOLDER='/opt/IOVisor-Data'
 
@@ -28,6 +39,6 @@ fi
 echo $FILE1
 echo $FILE2
 echo $FILE3
-sshpass -p $PASS scp {$FILE1,$FILE2,$FILE3} $USER@$HOST:$TARGETFOLDER
+sshpass -p $PASS scp {$FILE1,$FILE2,$FILE3} $VISIBILITY_CENTER_USER@$VISIBILITY_CENTER_IP:$TARGETFOLDER
 
 sudo rm -rf $FILE1 $FILE2 $FILE3
