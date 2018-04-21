db40269ad9f26412565e4f3c99e7a269f74ecb12
smartx-usman <usman@smartx.kr>
2018-04-21 16:11:52 +0900

    Only retain unique entries.

--- Visibility-Integration/tenant_subnet_tagger.sh
+++ Visibility-Integration/tenant_subnet_tagger.sh
@@ -14,38 +14,39 @@
 #  See the License for the specific language governing permissions and
 #
 # Name			: Get OpenStack Subnet List (Multi-region Deployment)
-# Description	: Script for Integating/Mapping neutron network, subnet and tenant for operator+tenant tagging to assist flowcentric visibility
+# Description	: Script for Integating/Mapping neutron network, subnet and tenant for operator+tenant tagging to assist flowcentric visibility. It must be executed in ID center.
 #
 # Created by    : Muhammad Usman
 # Version       : 0.1
-# Last Update	: February, 2018
+# Last Update	: April, 2018
 #
-# TH-CHULA
 
 OS_REGIONS="GIST1 KR-GIST2 GIST3 TW-NCKU MYREN MY-UM TH-CHULA VN-HUST"
 
 . /home/netcs/openstack/admin-openrc
-rm -rf tenant_subnet.temp1 tenant.tags
+rm -rf tenant_subnet.temp1 tenant.tags.temp tenant.tags
 
 for region in $OS_REGIONS
 do
-	openstack --os-region-name $region subnet list --no-dhcp -c Network -c Subnet -f csv --quote none > tenant_subnet.temp1
+        openstack --os-region-name $region subnet list --no-dhcp -c Network -c Subnet -f csv --quote none > tenant_subnet.temp1
 
-	while read -r subnetline
-	do
-		if [[ $subnetline == *"Network"* ]]; then
-			echo "Skip..."
-		else 
-			network_id=`echo $subnetline | cut -d ',' -f1`
-			subnet_address=`echo $subnetline | cut -d ',' -f2`
-			
-			subnetdetails=`openstack --os-region-name $region network show $network_id -c project_id -c provider:segmentation_id -f value`
-			project_id=`echo $subnetdetails | awk '{print $1}'`
-			vlan_id=`echo $subnetdetails | awk '{print $2}'`
-				
-			project_name=`openstack project list | grep  $project_id | cut -d'|' -f3 | awk '{$1=$1;print}'`
-			
-			echo "$vlan_id,$project_id,$project_name,$subnet_address" >> tenant.tags
-		fi
-	done < "tenant_subnet.temp1"
+        while read -r subnetline
+        do
+                if [[ $subnetline == *"Network"* ]]; then
+                        echo "Skip..."
+                else
+                        network_id=`echo $subnetline | cut -d ',' -f1`
+                        subnet_address=`echo $subnetline | cut -d ',' -f2`
+
+                        subnetdetails=`openstack --os-region-name $region network show $network_id -c project_id -c provider:segmentation_id -f value`
+                        project_id=`echo $subnetdetails | awk '{print $1}'`
+                        vlan_id=`echo $subnetdetails | awk '{print $2}'`
+
+                        project_name=`openstack project list | grep  $project_id | cut -d'|' -f3 | awk '{$1=$1;print}'`
+
+                        echo "$vlan_id,$project_id,$project_name,$subnet_address" >> tenant.tags.temp
+                fi
+        done < "tenant_subnet.temp1"
 done
+rm -rf tenant.tags
+cat tenant.tags.temp | sort -u > tenant.tags
