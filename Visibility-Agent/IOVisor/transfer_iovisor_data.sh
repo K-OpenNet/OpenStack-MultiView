1aed5b47f082cb5e1f436ff7ac2a9d8cc6f6e594
smartx-usman <usman@smartx.kr>
2018-06-29 17:14:15 +0900

    Modify data movement script.

--- Visibility-Agent/IOVisor/transfer_iovisor_data.sh
+++ Visibility-Agent/IOVisor/transfer_iovisor_data.sh
@@ -4,8 +4,8 @@
 # Description   : A script for transferring network packets data to the SmartX Visibility Center
 #
 # Created by    : Muhammad Usman
-# Version       : 0.2
-# Last Update   : May, 2018
+# Version       : 0.3
+# Last Update   : June, 2018
 
 #Modify these parameters before execution on SmartX Boxes
 #Also install sshpass and add SmartX Visibility Center IP for automatic logins.
@@ -13,30 +13,97 @@
 VISIBILITY_CENTER_IP=
 VISIBILITY_CENTER_USER=
 
-TARGETFOLDER='/home/netcs/IOVisor-Data/SmartX-Box-UM'
+TARGETFOLDER=
 SOURCEFOLDER='/opt/IOVisor-Data'
 
 Minute="$(date +'%M')"
 Hour="$(date +'%H')"
 cDate="$(date +'%Y-%m-%d')"
-if [ "$Minute" -le 30 ]
-then
-  if [ "$Hour" -eq 0 ]
-  then
-    Hour=23
-  else
-    Hour=$(( Hour-1 ))
-  fi
-  FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-30"
-  FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-30"
+
+if [ "$Hour" -lt 10 ]
+    then 
+        Hour="0$Hour"
+        echo $Hour
+fi
+
+if [ "$Minute" -eq 0 ]
+    then
+        PREVIOUS_HOUR=23
+        PREVIOUS_MINUTE=55
+        PREVIOUS_DAY="$(date +'%d')"
+        PREVIOUS_DAY=$((PREVIOUS_DAY - 1))
+        cDate="$(date +'%Y-%m')"
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$PREVIOUS_DAY-$PREVIOUS_HOUR-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$PREVIOUS_DAY-$PREVIOUS_HOUR-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 5 ]
+    then
+        PREVIOUS_MINUTE=00
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 10 ]
+    then
+        PREVIOUS_MINUTE=05
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 15 ]
+    then
+        PREVIOUS_MINUTE=10
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 20 ]
+    then
+        PREVIOUS_MINUTE=15
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 25 ]
+    then 
+        PREVIOUS_MINUTE=20
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE" 
+elif [ "$Minute" -eq 30 ]
+    then
+        PREVIOUS_MINUTE=25
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 35 ]
+    then
+        PREVIOUS_MINUTE=30
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 40 ]
+    then
+        PREVIOUS_MINUTE=35
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 45 ]
+    then 
+        PREVIOUS_MINUTE=40
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 50 ]
+    then
+        PREVIOUS_MINUTE=45
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+elif [ "$Minute" -eq 55 ]
+    then
+        PREVIOUS_MINUTE=50
+        PREVIOUS_FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-$PREVIOUS_MINUTE"
+        PREVIOUS_FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-$PREVIOUS_MINUTE"
+fi
+
+echo "$PREVIOUS_FILE1 $PREVIOUS_FILE2"
+
+sshpass -p $PASS scp $PREVIOUS_FILE1 $PREVIOUS_FILE2 $VISIBILITY_CENTER_USER@$VISIBILITY_CENTER_IP:$TARGETFOLDER
+sleep 20
+
+shopt -s extglob; set -H
+cd $SOURCEFOLDER
+CURRENT_FILES=`ls -t1 | head -n 2 | sed 'N; s/\n/|/'`
+sudo rm -v !($CURRENT_FILES)
+
+if [ $? -eq 0 ]; then
+    echo OK
 else
-  FILE1="/opt/IOVisor-Data/mc-$cDate-$Hour-00"
-  FILE2="/opt/IOVisor-Data/data-$cDate-$Hour-00"
-fi 
-
-echo $FILE1
-echo $FILE2
-
-sshpass -p $PASS scp {$FILE1,$FILE2} $VISIBILITY_CENTER_USER@$VISIBILITY_CENTER_IP:$TARGETFOLDER
-
-sudo rm -rf $FILE1 $FILE2
+    echo FAIL
+fi
