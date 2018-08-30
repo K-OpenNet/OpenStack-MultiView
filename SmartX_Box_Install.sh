b4924037acdf122a07471ba6f860a2f03f62cb2a
smartx-usman <usman@smartx.kr>
2018-08-30 14:53:49 +0900

    Code refactoring based on recent design semantics.

--- SmartX_Box_Install.sh
+++ SmartX_Box_Install.sh
@@ -13,12 +13,12 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #
-# Name          : Agent_Box_Install.sh
-# Description   : Script for Installing Dependencies on Client Box
+# Name          : SmartX_Box_Install.sh
+# Description   : Script for Installing Dependencies on Smart Box or SmartX Micro-Box
 #
 # Created by    : usman@smartx.kr
-# Version       : 0.1
-# Last Update   : December, 2016
+# Version       : 0.2
+# Last Update   : August, 2018
 
 #Update Management IP according to your requirement
 MGMT_IP=""
@@ -29,6 +29,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')][INFO][INSTALL] MultiView Client Box Agent Installation Started..."
+echo "[$(date '+%Y-%m-%d %H:%M:%S')][INFO][INSTALL] Multi-View Installation on SmartX Box is Started..."
 
 MultiView-Scripts/Install_Dependencies_vBox.sh "$MGMT_IP"
--- Visibility_Center_Install.sh
+++ Visibility_Center_Install.sh
@@ -13,12 +13,12 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #
-# Name          : Install_Dependencies.sh
-# Description   : Script for Installing Dependencies
+# Name          : Visibility_Center_Install.sh
+# Description   : Script for Installing Dependencies on SmartX Visibility Center.
 #
 # Created by    : usman@smartx.kr
-# Version       : 0.2
-# Last Update   : December, 2016
+# Version       : 0.3
+# Last Update   : August, 2018
 
 #Update Management IP according to your requirement
 MGMT_IP=""
@@ -29,7 +29,7 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')][INFO][INSTALL] MultiView Installation Started..."
+echo "[$(date '+%Y-%m-%d %H:%M:%S')][INFO][INSTALL] Multi-View Installation Started..."
 
 MultiView-Scripts/Install_Dependencies_vCenter.sh "$MGMT_IP"
 MultiView-Scripts/Create_MultiView_Database.sh "$MGMT_IP"
